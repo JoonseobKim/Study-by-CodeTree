38aa29bb9cdcf1dde62d592272a49b0e001b6da4
JoonseobKim <wnsjtq781@naver.com>
2026-09-10 13:51:00 +0900

    [Passed] 정수의 최솟값 (140ms, 10MB)

---
+++ 260910/정수의 최솟값/minimum-value-of-an-integer.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        // Please write your code here.
+        System.out.print(function(a, b, c));
+
+    }
+
+    public static int function(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
+    }
+}
