6f97fc2538f7009d996a4f6445433a41b77e1799
JoonseobKim <wnsjtq781@naver.com>
2026-09-10 13:37:59 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (120ms, 10MB)

---
+++ 260910/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        printStar();
+        printStar();
+        printStar();
+        printStar();
+        printStar();
+    }
+
+    public static void printStar() {
+        System.out.println("**********");
+    }
+}
