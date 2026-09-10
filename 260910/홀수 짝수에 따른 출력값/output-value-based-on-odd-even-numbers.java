3c396018b33ed9e909bc988f15030c8a4140d7dc
JoonseobKim <wnsjtq781@naver.com>
2026-09-10 17:13:25 +0900

    [Passed] 홀수 짝수에 따른 출력값 (126ms, 10MB)

---
+++ 260910/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        System.out.print(func(n));
+    }
+
+    public static int func(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+
+        return n + func(n - 2);
+
+    }
+}
