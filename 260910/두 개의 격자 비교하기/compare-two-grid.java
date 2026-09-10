12a4418c812882fbc6e83b17dbd7f8f6a9fa354e
JoonseobKim <wnsjtq781@naver.com>
2026-09-10 11:55:34 +0900

    [Passed] 두 개의 격자 비교하기 (152ms, 10MB)

---
+++ 260910/두 개의 격자 비교하기/compare-two-grid.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[][] arr1 = new int[N][M];
+        int[][] arr2 = new int[N][M];
+        for (int k = 0; k < 2; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (k == 0)
+                        arr1[i][j] = sc.nextInt();
+                    if (k == 1)
+                        arr2[i][j] = sc.nextInt();
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (arr1[i][j] == arr2[i][j])
+                    System.out.print("0 ");
+                else
+                    System.out.print("1 ");
+            }
+            System.out.println();
+        }
+
+    }
+}
