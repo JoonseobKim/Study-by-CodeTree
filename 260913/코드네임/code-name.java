79f2840d4b7f1dbef096931fcf65fc738ea6a9f0
JoonseobKim <wnsjtq781@naver.com>
2026-09-13 20:18:31 +0900

    [Passed] 코드네임 (130ms, 10MB)

---
+++ 260913/코드네임/code-name.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class User {
+    char codeName;
+    int score;
+}
+
+public class Main {
+    public static final int MAX_N = 5;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        User[] users = new User[MAX_N];
+        for (int i = 0; i < MAX_N; i++) {
+            users[i] = new User();
+            users[i].codeName = sc.next().charAt(0);
+            users[i].score = sc.nextInt();
+        }
+
+        // Please write your code here.
+        int minIdx = 0;
+
+        for (int i = 1; i < MAX_N; i++) {
+            if (users[i].score < users[minIdx].score) {
+                minIdx = i;
+            }
+        }
+
+        System.out.println(users[minIdx].codeName + " " + users[minIdx].score);
+    }
+}
